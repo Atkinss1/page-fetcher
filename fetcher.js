65ae0ae29b3199bf5d3ee3bfd52977762306c95b
Atkinss1 <jordanatkins@live.com>
2023-10-19 06:19:20 -0600

    add request function using CL arg, then write content to ./test.txt

--- fetcher.js
+++ fetcher.js
@@ -1,9 +1,21 @@
 const request = require('request');
 const fs = require('fs');
+
+// command line arguments that takes the index from an array to use in our request function
 const fileAddress = process.argv.slice(2)[0].trim();
 
-// request(fileAddress, (error, response, body) => {
-//   console.log('error:', error);
-//   console.log('Status code:', response);
-//   console.log('body:', body);
-// });
+
+
+request(fileAddress, (error, response, body) => {
+  if (!error) {
+    fs.writeFile('./test.txt', body, (err) => {
+      if (err) {
+        console.log('There was an error: ', err);
+      } else {
+        console.log('File written successfully.');
+      }
+    });
+  } else {
+    console.log('There was an error: ', error);
+  }
+});
